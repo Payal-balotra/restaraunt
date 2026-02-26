72ea40969e75ade7807ebc92383d971af607e7c1
Payal-balotra <balotra123.singh@gmail.com>
2026-02-26 14:40:59 +0530

    Delete dist directory

--- dist/utils/catchAsync.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = catchAsync;
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-function catchAsync(fn) {
-  return function (req, res, next) {
-    Promise.resolve(fn(req, res, next)).catch(next);
-  };
-}
